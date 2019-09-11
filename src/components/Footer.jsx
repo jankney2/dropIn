33efb6a3c2d2275bf06924be924c3a841db855bb
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2-010037070015.app.byu.edu>
2019-09-11 13:11:28 -0600

    inshallah

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ export default function Footer(){
     <div className="footer">
       <ul>
       <li>Copyright 2019 Nakit Development</li>
+      <li>Version 1.0.0</li>
         <li>3900 Adobe Way, Lehi, UT 84043</li>
       </ul>
     </div>
